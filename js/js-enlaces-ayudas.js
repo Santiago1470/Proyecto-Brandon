9333939bf697cbcc505db396f2316c65f7a99ae7
Santiago1470 <67245823+Santiago1470@users.noreply.github.com>
2022-10-14 18:20:33 -0500

    Add files via upload

---
+++ js/js-enlaces-ayudas.js
@@ -0,0 +1,56 @@
+
+function ocultar1(){
+    document.getElementById("flecha1-negra").style.display = "none";
+    document.getElementById("flecha1-blanca").style.display = "block";
+}
+
+function mostrar1(){
+    document.getElementById("flecha1-negra").style.display = "block";
+    document.getElementById("flecha1-blanca").style.display = "none";
+}
+
+function ocultar2(){
+    document.getElementById("flecha2-negra").style.display = "none";
+    document.getElementById("flecha2-blanca").style.display = "block";
+}
+
+function mostrar2(){
+    document.getElementById("flecha2-negra").style.display = "block";
+    document.getElementById("flecha2-blanca").style.display = "none";
+}
+function ocultar3(){
+    document.getElementById("flecha3-negra").style.display = "none";
+    document.getElementById("flecha3-blanca").style.display = "block";
+}
+
+function mostrar3(){
+    document.getElementById("flecha3-negra").style.display = "block";
+    document.getElementById("flecha3-blanca").style.display = "none";
+}
+function ocultar4(){
+    document.getElementById("flecha4-negra").style.display = "none";
+    document.getElementById("flecha4-blanca").style.display = "block";
+}
+
+function mostrar4(){
+    document.getElementById("flecha4-negra").style.display = "block";
+    document.getElementById("flecha4-blanca").style.display = "none";
+}
+function ocultar5(){
+    document.getElementById("flecha5-negra").style.display = "none";
+    document.getElementById("flecha5-blanca").style.display = "block";
+}
+
+function mostrar5(){
+    document.getElementById("flecha5-negra").style.display = "block";
+    document.getElementById("flecha5-blanca").style.display = "none";
+}
+function ocultar6(){
+    document.getElementById("flecha6-negra").style.display = "none";
+    document.getElementById("flecha6-blanca").style.display = "block";
+}
+
+function mostrar6(){
+    document.getElementById("flecha6-negra").style.display = "block";
+    document.getElementById("flecha6-blanca").style.display = "none";
+}
